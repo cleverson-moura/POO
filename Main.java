587f3f7987bd13f35a393980c1a28b2493ef5a91
Cleverson Moura <cleversonmoura178@gmail.com>
2022-04-20 19:03:38 -0300

    first commit

--- Main.java
+++ Main.java
@@ -11,26 +11,16 @@ public class Main {
             cont = Integer.parseInt(JOptionPane.showInputDialog("Digite sua opção\n1 - ligar\n2 - Desligar\n3 - Aumentar volume\n4 - Diminuir volume\n5 - Aumentar canal\n6 - Diminuir canal\n0 - Sair"));
             if(cont == 1){
                 tv.ligar();
-                System.out.println(tv.ld);
             }else if(cont == 2){
                 tv.desligar();
-                System.out.println(tv.ld);
             }else if(cont == 3){
                 tv.volumeA();
-                System.out.println("O volume é:");
-                System.out.println(tv.volume);
             }else if(cont == 4){
                 tv.volumeD();
-                System.out.println("O volume é:");
-                System.out.println(tv.volume);
             }else if(cont == 5){
                 tv.canalA();
-                System.out.println("O canal é:");
-                System.out.println(tv.canal);
             }else if(cont == 6){
                 tv.canalD();
-                System.out.println("O canal é:");
-                System.out.println(tv.canal);
             }
         }
     }
--- Tv.java
+++ Tv.java
@@ -1,37 +1,43 @@
 package com.company;
 
 public class Tv {
-    boolean ld;
-    int volume;
-    int canal;
+    private boolean ld;
+    private int volume;
+    private int canal;
 
 //Liga e desliga
 
     public void ligar(){
         ld = true;
+        System.out.println(ld);
     }
 
     public void desligar(){
         ld = false;
+        System.out.println(ld);
     }
 
 //Altera o volume
 
     public void volumeA(){
         volume ++;
+        System.out.println("O volume é:"+volume);
     }
 
     public void volumeD(){
         volume --;
+        System.out.println("O volume é:"+volume);
     }
 
 //Altera o canal
 
     public void canalA(){
         canal ++;
+        System.out.println("O canal é:"+canal);
     }
 
     public void canalD(){
         canal --;
+        System.out.println("O canal é:"+canal);
     }
 }
